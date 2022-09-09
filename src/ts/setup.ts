51fab74c6478d93d0b44c00e77a03466c94623f5
Miklakapi <kkmiklaszewski@gmail.com>
2022-09-09 19:12:24 +0000

    init

--- src/ts/setup.ts
+++ src/ts/setup.ts
@@ -1,1 +1,9 @@
-alert('hello');
+"use strict";
+
+$(document).ready(function () {
+    for (let x = 0; x < 20; x++) {
+        for (let y = 0; y < 20; y++) {
+            $('.area').append('<div class="square"></div>');
+        }
+    }
+});
