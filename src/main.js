b7733dcb3761af981a9c0a3d2a1245c2d2722102
Miklakapi <kkmiklaszewski@gmail.com>
2022-09-28 17:45:29 +0000

    game switch

--- src/main.js
+++ src/main.js
@@ -1,11 +1,25 @@
 import './App/style.scss';
 import AppController from './App/Controllers/AppController';
 
-import './Minesweeper/style.scss';
 import './Snake/style.scss';
+import SnakeController from './Snake/Controllers/SnakeController';
 
+import './Minesweeper/style.scss';
+import MinesweeperController from './Minesweeper/Controllers/MinesweeperController';
+
+
+const Games = {
+    Minesweeper: MinesweeperController,
+    Snake: SnakeController
+};
+
+let currentGame = null;
 
 $(document).ready(function () {
     const appController = new AppController();
-    
+}).on('click', '.play', function () {
+    const game = $(this).data('game');
+    currentGame = new (Games[game])();
+}).on('click', '.arrow', function () {
+    currentGame = null;
 });
