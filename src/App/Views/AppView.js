7fb2b168a54e5ac926fbf7b12130ab48502d1897
Miklakapi <kkmiklaszewski@gmail.com>
2022-09-28 16:37:13 +0000

    animations

--- src/App/Views/AppView.js
+++ src/App/Views/AppView.js
@@ -1,13 +1,25 @@
 class AppView {
-    #arrow = $('.arrow');
-    #wrapper = $('.wrapper');
+    #arrows = $('.arrow');
+    #title = $('.title h1');
+    #app = $('.app');
 
     addHandlerSwitchGame(handler) {
-        this.#arrow.on("click", function () {
-            
-            handler();
+        this.#arrows.on("click", (event) => {
+            const target = $(event.target);
+            const direction = target.data('direction');
+            this.#title.addClass('animate-rotate');
+            this.#app.addClass('animate-shake');
+            setTimeout(() => {
+                this.#title.removeClass('animate-rotate');
+                this.#app.removeClass('animate-shake');
+            }, 500);
+            handler(direction);
         });
+    }
+
+    changeTitle(text) {
+        this.#title.html(text);
     }
 }
 
-export default new AppView();
+export default AppView;
