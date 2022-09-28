26d7d4ceaf97a3220beede000a379e434bbcbd2f
Miklakapi <kkmiklaszewski@gmail.com>
2022-09-28 18:56:14 +0000

    snake enum

--- src/Snake/Models/SnakeModel.js
+++ src/Snake/Models/SnakeModel.js
@@ -1,5 +1,12 @@
 class SnakeModel {
+    static Direction = {
+        ArrowUp: [0, -1],
+        ArrowRight: [1, 0],
+        ArrowDown: [0, 1],
+        ArrowLeft: [-1, 0]
+    }
 
+    #direction = null;
 }
 
 export default SnakeModel;
